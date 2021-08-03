e44eccc54a46caafcc8b23b0179d9953d0714295
caokhacdung <caokhacdung2018@gmail.com>
2021-08-03 16:20:31 +0700

    prog

---
+++ kieu-du-lieu/kdl.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+    int a;
+	float b;
+	char c;
+	short s;
+	double d;
+	unsigned int ui;
+	long long ll;
+int main (){
+	
+	//sizeof (toan tu)
+	printf("size of char is %d\n", sizeof c);
+	printf("size of int is %d\n", sizeof a);
+	printf("size of float is %d\n", sizeof b);
+	printf("size of short is %d\n", sizeof s);
+	printf("size of double is %d\n", sizeof d);
+	printf("size of unsigned int is %d\n", sizeof ui);
+	printf("size of long long is %d\n", sizeof ll);
+}
---
+++ odd-even-qualification-program/Untitled1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf ("nhap so a = ");
+	scanf ("%d", &a);
+	if (a % 2 == 0){
+		printf ("so ban moi nhap la so chan");
+	}else{
+		printf ("so ban moi nhap la so le");
+	}
+}
